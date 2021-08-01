b0f8a8cfcd8132674a1dbd031bba83e4728e8289
wandsX <ywwithpeople@gmail.com>
2021-08-02 00:07:20 +0800

    BinaryTree

---
+++ leetcode/editor/cn/144-binary-tree-preorder-traversal.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include "include/TreeNode.h"
+
+using namespace std;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+class Solution
+{
+    public:
+        vector<int> preorderTraversal(TreeNode *root)
+        {
+            std::vector<int> res;
+            travese(root, res);
+
+            return res;
+        }
+
+        void travese(TreeNode *cur, vector<int> &res)
+        {
+            if (cur == nullptr)
+            { return; }
+
+            res.push_back(cur->val);
+            travese(cur->left, res);
+            travese(cur->right, res);
+        }
+};
+//leetcode submit region end(Prohibit modification and deletion)
+
